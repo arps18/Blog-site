5d3724c590af3e8dcb75aca615398c309ee6f528
arps18 <arpanp99@gmail.com>
2021-05-16 21:56:35 +0530

    Added Footer Changes

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <div>
          
-<footer class="footer bg-white relative pt-1 border-b-2 border-blue-700">
+<footer class="footer  bg-white relative pt-1 border-b-2 border-blue-700">
   
     
         <div class="mt-2 border-t-2 border-gray-300 flex flex-col items-center">
